1ca565f1cd2dc240ec53247036c26f294d31f5a7
Reprak11 <reynaldo.armenta@cetys.edu.mx>
2022-02-10 00:00:44 -0800

    Django Templates Tutorial

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,12 +1,17 @@
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Hello world from Django, Reprak11</h1>")
+def home(response):
+    return render(response,"main/home.html", {})
 
 def GetAList(response, id):
     ls = ToDoList.objects.get(id=id)
-    items = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, items.text))
+    my_dict = {
+        "ls":ls
+    }
+    #items = ls.item_set.get(id=1)
+    #return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, items.text))
+    return render(response,"main/list.html", my_dict)

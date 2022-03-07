5f9c6aac8813c57a9c5333ed8b478fa836ce904d
Reprak11 <reynaldo.armenta@cetys.edu.mx>
2022-03-07 00:48:41 -0800

    Tutorial Simple Forms

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("",views.home, name="home"),
-    path("<int:id>", views.GetAList,name="GetAList")
+    path("<int:id>", views.GetAList,name="GetAList"),
+    path("create/", views.create,name="create")
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,7 +1,8 @@
 import re
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
+from .forms import CreateNewList
 # Create your views here.
 
 def home(response):
@@ -12,6 +13,20 @@ def GetAList(response, id):
     my_dict = {
         "ls":ls
     }
+    print(my_dict["ls"].item_set.all())
     #items = ls.item_set.get(id=1)
     #return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, items.text))
     return render(response,"main/list.html", my_dict)
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+
+        return HttpResponseRedirect("/%i" % t.id)
+    else:
+        form = CreateNewList()
+    return render(response,"main/create.html", {"form":form})
